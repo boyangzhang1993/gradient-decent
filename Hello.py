ab1165bd9e615032996a7b33b6cdc00856a828fc
Boyang Zhang <boyaz14@uci.edu>
2024-05-01 22:07:57 +0000

    inint

--- Hello.py
+++ Hello.py
@@ -1,24 +1,113 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 import streamlit as st
 from streamlit.logger import get_logger
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd
+from typing import Union, List
+import time
+import streamlit.components.v1 as components
+
 
 LOGGER = get_logger(__name__)
 
+# https://i.imgflip.com/8oo1fb.jpg
+
+def show_image_in_expander(url='https://i.imgflip.com/8oo1fb.jpg'):
+    with st.expander("Click to view the image"):
+        st.image(url, caption="Here's your image!")
+        
+def show_temporary_image(url='https://i.imgflip.com/8oo1fb.jpg', timeout=5):
+    # Display the image using the URL
+    st.image(url, )
+    
+    # Wait for 'timeout' seconds
+    # time.sleep(timeout)
+    
+    # Clear the previous output (the image)
+    # st.experimental_rerun()
+    
+def cosine_similarity_matrix_formal(X:List[float], Y:List[float]) -> np.ndarray:
+    x = np.array(X)
+    y = np.array(Y)
+    x_norm = np.sqrt(np.sum(x**2))
+    y_norm = np.sqrt(np.sum(y**2, axis=1, keepdims=True))
+    return np.dot(x, y.T) / (x_norm * y_norm)
+
+# def plot_function(f, x_range, current_x):
+#     x = np.linspace(x_range[0], x_range[1], 400)
+#     y = eval(f)
+#     plt.figure(figsize=(10, 5))
+#     plt.plot(x, y, label=f'f(x) = {f}')
+#     plt.scatter([current_x], [eval(f.replace('x', f'({current_x})'))], color='red')  # Plot current point
+#     plt.title("Function Graph")
+#     plt.xlabel("x")
+#     plt.ylabel("f(x)")
+#     plt.legend()
+#     plt.grid(True)
+#     plt.close()
+#     return plt
+
+
+
+
+single_cell_df = pd.DataFrame({
+    'gene_1': [1, 0, 0],
+    'gene_2': [2, 0, 0],
+    'gene_3': [3, 0, 0],
+    'gene_4': [0, 1, 0],
+    'gene_5': [0, 2, 0],
+    'gene_6': [0, 3, 0],
+    'gene_7': [0, 0, 1],
+    'gene_8': [0, 0, 2],
+    'gene_9': [0, 0, 3],
+    },
+    index=['cell_A', 'cell_B', 'cell_C'],
+)
+def simulate_tangram_prediction_groundtruth(
+    classic_example: bool = False,
+    single_cell_df: pd.DataFrame = single_cell_df,
+    # single_cell_adata: AnnData = single_cell_adata,
+    seed: Union[None, int] = None,
+    size_spot: int = 3,
+):
+
+    if seed:
+        np.random.seed(seed)
+
+    l_spot = ['spot_' + str(i) for i in range(size_spot)]
+    simulate_spatial = pd.DataFrame(
+        np.random.rand(size_spot, 3),  # Random a 3x3 matrix
+        columns=['cell_A', 'cell_B', 'cell_C'],
+        index=l_spot,
+    )
+
+    if classic_example:
+        simulate_spatial = pd.DataFrame(
+            [[0.5, 0, 0.5,]],  # Random a 3x3 matrix
+            columns=['cell_A', 'cell_B', 'cell_C'],
+            index=['spot_1'],
+        )
+
+    single_cell_df = single_cell_df.reindex(simulate_spatial.columns)
+
+
+
+    simulate_spatial_data = single_cell_df.T.dot(simulate_spatial.T).T
+
+
+    matrix_groundtruth = round(simulate_spatial, 5)
+
+
+
+    return matrix_groundtruth, simulate_spatial_data
+
 
 def run():
+    
+
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
@@ -34,18 +123,96 @@ def run():
         Machine Learning and Data Science projects.
         **👈 Select a demo from the sidebar** to see some examples
         of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
     """
     )
+    st.subheader('This is the Single Cell Data')
+    st.dataframe(data=single_cell_df)
+
+    ## 
+    matrix_groundtruth, simulate_spatial_data = simulate_tangram_prediction_groundtruth(classic_example=True,
+                                                                                        size_spot=1,)
+    st.subheader('This is the Simulated Visium Data')
+    
+    if st.button('Show/Hide Grouth Truth'):
+        # Toggle the state
+        if 'show_df' not in st.session_state:
+            st.session_state.show_df = True
+        else:
+            st.session_state.show_df = not st.session_state.show_df
+    if st.session_state.get('show_df', False):
+        st.dataframe(data=matrix_groundtruth)
+        st.write("This ground truth ")
+    # st.dataframe(data=matrix_groundtruth)
+    st.write("So we use ground truth to simulate a Visium data")
+    st.dataframe(data=simulate_spatial_data)
+    # Prediction
+    st.subheader('This is a Prediction we are using', divider='rainbow')
+    
+    a = st.slider('Select value for a', min_value=-100, max_value=100, value=1)
+    b = st.slider('Select value for b', min_value=-100, max_value=100, value=10)
+    c = st.slider('Select value for c', min_value=-100, max_value=100, value=1)
+
+    # Create the matrix using numpy
+    
+    M_matrix = np.array([[a, b, c]])
+    min_vals = M_matrix.min(axis=1)
+    max_vals = M_matrix.max(axis=1)
+
+    M_matrix = (M_matrix - min_vals) / (max_vals - min_vals)
+    # Display the matrix
+    # st.write('Normalized Matrix M (proportion of cells):', )
+    # st.
+    st.write('#### Normalized a, b, c to represent cell proportion')
+    st.dataframe(data=pd.DataFrame(M_matrix, 
+                                   columns=['cell_A','cell_B','cell_C']),
+                 hide_index=True)
+    st.write('#### So according to cell proportion, below is the predicted gene profiles')
+    prediction_genes = M_matrix.dot(single_cell_df)
+    st.dataframe(data=pd.DataFrame(prediction_genes,
+                                   columns=['gene_1','gene_2','gene_3','gene_4','gene_5','gene_6','gene_7','gene_8','gene_9'],
+                                   index=['spot_1']),
+                 hide_index=True)
+    if st.button('Show/Hide Visium'):
+        # Toggle the state
+        if 'show_df' not in st.session_state:
+            st.session_state.show_df = True
+        else:
+            st.session_state.show_df = not st.session_state.show_df
+        if st.session_state.get('show_df', False):
+            st.write("Visium data we got")
+            st.dataframe(data=simulate_spatial_data)
+            
+
+    cosine_init = cosine_similarity_matrix_formal(prediction_genes, simulate_spatial_data)
+    
+    
+    if cosine_init > 0.9:
+        st.success(f'Match found ! Similarity is {cosine_init[0][0]}')
+        st.toast('You did good job on Prediction!', icon='🎉')
+        # mycode = "<a href='https://imgflip.com/i/8oo1fb'><img src='https://i.imgflip.com/8oo1fb.jpg' /></a><div><a href='https://imgflip.com/memegenerator'>from Imgflip Meme Generator</a></div>"
+        # components.html(mycode, height=0, width=0)
+        st.balloons()
+
+        # show_image_in_expander()
+        show_temporary_image()
+    elif cosine_init > 0.5:
+        st.info(f'Ok, try it again ! Similarity is {cosine_init[0][0]}')
+        # st.snow()
+
+    else:
+        st.error(f'Well, we can do better ! Similarity is {cosine_init[0][0]}')
+        
+    
+    
+    st.caption(f'The similarity of prediction and truth (Cosine similiarity): {cosine_init[0][0]}')
+    
+    
+    # 
+    
+
 
 
 if __name__ == "__main__":
+    
+    
     run()
--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -1,77 +1,363 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-from urllib.error import URLError
 
-import altair as alt
 import pandas as pd
-
+import numpy as np
 import streamlit as st
-from streamlit.hello.utils import show_code
+import plotly.express as px
+from typing import Union, List
+import copy
+import plotly.graph_objects as go
+import time
+
+def cosine_similarity_matrix_formal(X:List[float], Y:List[float]) -> np.ndarray:
+    x = np.array(X)
+    y = np.array(Y)
+    x_norm = np.sqrt(np.sum(x**2))
+    y_norm = np.sqrt(np.sum(y**2, axis=1, keepdims=True))
+    return np.dot(x, y.T) / (x_norm * y_norm)
+
+def cosine_loss_autograd(a: float, 
+                         b: float, 
+                         c: float, Y
+                         : np.ndarray, supply_x:Union[np.ndarray, None]=None) -> float:
+    x = np.array([[a, b, c]])
+    if supply_x is not None:
+        M_matrix = np.array([[a, b, c]])
+        x = np.array(M_matrix.dot(supply_x))
+
+    similarity = cosine_similarity_matrix_formal(x, Y)
+    return 1 - np.mean(similarity)
+
+def derivative_approx(
+    arg_choose: str = 'a', 
+    const: float = 0.0001, 
+    a: float = 1, 
+    b: float = 1, 
+    c: float = 1, 
+    X: Union[np.ndarray, None] = None,
+    Y: np.ndarray = np.array([[1, 1, 1]]
+                             
+                             )
+) -> float:
+    assert arg_choose in {'a', 'b', 'c'}
+
+    params = {
+        'a': a,
+        'b': b,
+        'c': c,
+        'supply_x': X,
+        'Y': Y,
+
+    }
+
+    params_add = copy.deepcopy(params)
+    params_delete = copy.deepcopy(params)
+
+    params_add[arg_choose] += const
+    # print(params_add[arg_choose])
+
+    params_delete[arg_choose] -= const
+    # print(params_delete[arg_choose])
+    gradient_approx = (cosine_loss_autograd(**params_add) - 
+                       cosine_loss_autograd(**params_delete)) / (2*const)
+
+    return gradient_approx
+
+single_cell_df = pd.DataFrame({
+    'gene_1': [1, 0, 0],
+    'gene_2': [2, 0, 0],
+    'gene_3': [3, 0, 0],
+    'gene_4': [0, 1, 0],
+    'gene_5': [0, 2, 0],
+    'gene_6': [0, 3, 0],
+    'gene_7': [0, 0, 1],
+    'gene_8': [0, 0, 2],
+    'gene_9': [0, 0, 3],
+    },
+    index=['cell_A', 'cell_B', 'cell_C'],
+)
+def simulate_tangram_prediction_groundtruth(
+    classic_example: bool = False,
+    single_cell_df: pd.DataFrame = single_cell_df,
+    # single_cell_adata: AnnData = single_cell_adata,
+    seed: Union[None, int] = None,
+    size_spot: int = 3,
+):
+
+    if seed:
+        np.random.seed(seed)
+
+    l_spot = ['spot_' + str(i) for i in range(size_spot)]
+    simulate_spatial = pd.DataFrame(
+        np.random.rand(size_spot, 3),  # Random a 3x3 matrix
+        columns=['cell_A', 'cell_B', 'cell_C'],
+        index=l_spot,
+    )
+
+    if classic_example:
+        simulate_spatial = pd.DataFrame(
+            [[0.5, 0, 0.5,]],  # Random a 3x3 matrix
+            columns=['cell_A', 'cell_B', 'cell_C'],
+            index=['spot_1'],
+        )
+
+    single_cell_df = single_cell_df.reindex(simulate_spatial.columns)
+
+
+
+    simulate_spatial_data = single_cell_df.T.dot(simulate_spatial.T).T
+
+
+    matrix_groundtruth = round(simulate_spatial, 5)
+
+
+
+    return matrix_groundtruth, simulate_spatial_data
 
 
 def data_frame_demo():
-    @st.cache_data
-    def get_UN_data():
-        AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-        df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-        return df.set_index("Region")
 
-    try:
-        df = get_UN_data()
-        countries = st.multiselect(
-            "Choose countries", list(df.index), ["China", "United States of America"]
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+
+    
+    
+    max_iterations = 100000 # To avoid infinite loop
+    cos_target = 0.98 # Target cosine similarity
+    learning_rate = 0.1 # Learning rate
+
+    # Reset 
+
+    a=1
+    b=10
+    c=-1
+    matrix_groundtruth, simulate_spatial_data = simulate_tangram_prediction_groundtruth(classic_example=True,
+                                                                                            size_spot=1,)
+
+    l_params = ['a', 'b', 'c']
+    params_gradients = {
+        'arg_choose': 'a',
+        'a':a,
+        'b':b,
+        'c':c,
+        'X':single_cell_df,
+        'Y':simulate_spatial_data,
+        
+
+    }
+    a_values, b_values, c_values, cos_values = [], [], [], []
+    grad_a, grad_b, grad_c = [], [], []
+    # Start the loop
+    
+
+    for _ in range(max_iterations):
+        
+        # new_rows = last_rows[-1, :] + np.random.randn(1, 1).cumsum(axis=0)
+        status_text.text("%i%% Complete" % _)
+
+
+        M_matrix = np.array([[params_gradients['a'], 
+                            params_gradients['b'], 
+                            params_gradients['c']]])
+        min_vals = M_matrix.min(axis=1)
+        max_vals = M_matrix.max(axis=1)
+
+        M_matrix = (M_matrix - min_vals) / (max_vals - min_vals)
+        params_gradients['a'] = M_matrix[0][0]
+        params_gradients['b'] = M_matrix[0][1]
+        params_gradients['c'] = M_matrix[0][2]
+
+        
+        prediction_genes = M_matrix.dot(single_cell_df)
+
+        cos_value = cosine_similarity_matrix_formal(prediction_genes, simulate_spatial_data)
+        
+        print(f'Cosine similarity: {cos_value}')
+        if cos_value > cos_target:
+            break
+
+        # Capture current values
+        a_values.append(params_gradients['a'])
+        b_values.append(params_gradients['b'])
+        c_values.append(params_gradients['c'])
+        cos_values.append(cos_value)
+        
+        dict_gradients = {}
+        
+        max_gradient_iteration = float('-inf')
+        for i, param in enumerate(l_params):
+            params_gradient_test = {
+                'arg_choose': param,
+                'a':params_gradients['a'],
+                'b':params_gradients['b'],
+                'c':params_gradients['c'],
+                'X':single_cell_df,
+                'Y':simulate_spatial_data,
+            }
+            gradient_approx = derivative_approx(**params_gradient_test)
+
+            if param == 'a':
+                grad_a.append(gradient_approx)
+            elif param == 'b':
+                grad_b.append(gradient_approx)
+            elif param == 'c':
+                grad_c.append(gradient_approx)
+            params_gradients[param] -= learning_rate * gradient_approx
+
+
+            if gradient_approx > max_gradient_iteration:
+                max_gradient_iteration = gradient_approx
+                max_gradient_param = param
+            dict_gradients[param] = gradient_approx
+            
+            
+            
+
+    df_countour = df = pd.DataFrame({
+        'a': a_values,
+        'b': b_values,
+        'c': c_values,
+        'grad_a': grad_a,
+        'grad_b': grad_b,
+        'grad_c': grad_c,
+        'cosine_similarity': [v[0][0] for v in cos_values],
+        
+    })
+    data = pd.DataFrame([[float(a),float(b),float(c),
+                        #   float(grad_a[0]),
+                        #   float(grad_b[0]),
+                        #   float(grad_c[0]),
+                        #   float([v[0][0] for v in cos_values][0]),
+                          ]
+                         
+                         ],columns=["A", "B", "C", 
+                                    # "Gradient_A", "Gradient_B", "Gradient_C", 'Cosine similarity'
+                                    ])
+
+    my_data_element = st.line_chart(data, color=['#1F77B4', 
+                                                  '#D62728', 
+                                                  '#E377C2'])
+    for i, row in df.iterrows():
+        a = float(row['a'])
+        b = float(row['b'])
+        c = float(row['c'])
+        a_g = float(row['grad_a'])
+        b_g = float(row['grad_b'])
+        c_g = float(row['grad_c'])
+        c_s = float(row['cosine_similarity'])
+        
+        
+        # add_df = pd.DataFrame([[float(M_matrix[0][0]),float(M_matrix[0][0]),float(M_matrix[0][0])
+        #                         ]
+        #                        ], columns=(["A", "B", "C"]))
+        # my_data_element.add_rows(add_df)
+        # st.write(f'{a}')
+        
+        add_df = pd.DataFrame(np.array([[a,b,c,a_g,b_g,c_g,c_s]]), columns=["A", "B", "C", "Gradient_A", "Gradient_B", "Gradient_C", 'Cosine similarity'])
+        my_data_element.add_rows(add_df)  # Correctly using add_rows on a dataframe element
+        progress_bar.progress(_)
+        # last_rows = new_rows
+        time.sleep(0.05)
+    data = pd.DataFrame([[
+        # float(a),float(b),float(c),
+                        float(grad_a[0]),
+                        float(grad_b[0]),
+                        float(grad_c[0]),
+                        float([v[0][0] for v in cos_values][0]),
+                        ]
+                        
+                        ],columns=[
+                            # "A", "B", "C", 
+                            "Gradient_A", "Gradient_B", "Gradient_C", 'Cosine similarity'])
+
+    my_data_element = st.line_chart(data)
+    for i, row in df.iterrows():
+        a = float(row['a'])
+        b = float(row['b'])
+        c = float(row['c'])
+        a_g = float(row['grad_a'])
+        b_g = float(row['grad_b'])
+        c_g = float(row['grad_c'])
+        c_s = float(row['cosine_similarity'])
+        
+        
+        # add_df = pd.DataFrame([[float(M_matrix[0][0]),float(M_matrix[0][0]),float(M_matrix[0][0])
+        #                         ]
+        #                        ], columns=(["A", "B", "C"]))
+        # my_data_element.add_rows(add_df)
+        # st.write(f'{a}')
+        
+        add_df = pd.DataFrame(np.array([[
+            # a,b,c,
+                                         a_g,b_g,c_g,c_s]]), columns=[
+                                            #  "A", "B", "C", 
+                                             "Gradient_A", "Gradient_B", "Gradient_C", 'Cosine similarity'])
+        my_data_element.add_rows(add_df)  # Correctly using add_rows on a dataframe element
+        progress_bar.progress(_)
+        # last_rows = new_rows
+        time.sleep(0.05)
+        
+        
+        
+    fig1 = go.Figure(data=[go.Scatter3d(
+        x=df['c'],
+        y=df['b'],
+        z=df['cosine_similarity'],
+        mode='markers',
+        marker=dict(
+            size=5,
+            color=df['cosine_similarity'],  # set color to cosine_similarity
+            colorscale='Viridis',  # choose a colorscale
+            opacity=0.8
         )
-        if not countries:
-            st.error("Please select at least one country.")
-        else:
-            data = df.loc[countries]
-            data /= 1000000.0
-            st.write("### Gross Agricultural Production ($B)", data.sort_index())
-
-            data = data.T.reset_index()
-            data = pd.melt(data, id_vars=["index"]).rename(
-                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-            )
-            chart = (
-                alt.Chart(data)
-                .mark_area(opacity=0.3)
-                .encode(
-                    x="year:T",
-                    y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                    color="Region:N",
-                )
-            )
-            st.altair_chart(chart, use_container_width=True)
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
-            Connection error: %s
-        """
-            % e.reason
+    )])
+    
+    fig1.update_layout(
+    title='3D Scatter Plot of Cosine Similarity',
+    scene=dict(
+        xaxis_title='c',
+        yaxis_title='b',
+        zaxis_title='Cosine Similarity'
+    ),
+    margin=dict(l=0, r=0, b=0, t=50)
+    )
+    
+    fig2 = go.Figure(data=[go.Scatter3d(
+        x=df['a'],
+        y=df['b'],
+        z=df['cosine_similarity'],
+        mode='markers',
+        marker=dict(
+            size=5,
+            color=df['cosine_similarity'],
+            colorscale='Viridis',
+            opacity=0.8
         )
+    )])
+    fig2.update_layout(
+        title='3D Scatter Plot of Cosine Similarity',
+        scene=dict(
+            xaxis_title='a',
+            yaxis_title='b',
+            zaxis_title='Cosine Similarity'
+        ),
+        margin=dict(l=0, r=0, b=0, t=50)
+    )
 
+    # Streamlit layout for side-by-side plots
+    col1, col2 = st.columns(2)
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
-st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
-)
+    # Displaying the figure in Streamlit
+    with col1:
+        st.plotly_chart(fig1, use_container_width=True)
+    with col2:
+        st.plotly_chart(fig2, use_container_width=True)
+
 
 data_frame_demo()
 
-show_code(data_frame_demo)
+
+
+
+
+# show_code(data_frame_demo)
